7d5cab725f426343e3516866ea2fe269c916f330
Salvador Delgado <sdelgado84@gmail.com>
2018-03-08 02:54:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS [
+def WIN_COMBINATIONS [
  [0,1,2], #Top row
  [3,4,5], #Middle row
  [6,7,8], #Bottom row
